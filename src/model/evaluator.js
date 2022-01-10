14e7b8b80dfaee7629cb2b374491282438c71016
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-01-10 10:48:14 +0200

    .

--- src/model/evaluator.js
+++ src/model/evaluator.js
@@ -1,3 +1,19 @@
+function evalExpr(input, values) {
+   let lexems = lexical(input);
+   let poland = toPoland(lexems);
+   let result = evalPoland(poland, ops, values);
+   return result;
+}
+
+function evalConst(input) {
+   if (input.indexOf(',') != -1 || input.indexOf('/') != -1) {
+      let rows = input.split("/").filter(r => r.trim());
+      let v = rows.map(r => r.split(',').map(x => new Complex(x)));
+      return v; 
+   }   
+   return new Complex(input);       
+}
+
 
 function evalPoland(poland, ops, vals) {
    const stack = [];
